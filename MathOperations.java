994b5050eeefd46d5c80a5c5c2b8a9740464fe27
Manjil <manjildangol81@gmail.com>
2025-11-19 13:13:10 +0545

    Initial sharing of project

---
+++ MathOperations.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class MathOperations here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MathOperations
+{
+    public static void main(String[] argus ){
+        int a=15;
+        int b=3;
+        
+        //arithematic operator
+        int add=a+b;
+        int sub=a-b;
+        int prod=a*b;
+        int div=a/b;
+        
+        //output
+        System.out.println("Addition="+add+"Subtraction="+sub+"Product="+prod+"Division"+
+    }
+}
